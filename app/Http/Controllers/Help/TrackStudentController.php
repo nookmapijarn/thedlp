6cc112417ecf4422415ff6e1c54945773ce63d9e
nookmapijarn <nookmapijarn@gmail.com>
2023-06-13 16:09:07 +0700

    redesign welcome

--- app/Http/Controllers/Help/TrackStudentController.php
+++ app/Http/Controllers/Help/TrackStudentController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers\Help;
 
+
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
 use Illuminate\Support\Facades\DB;
 
 class TrackStudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
 
-    public function index(Request $request)
+    public function create()
     {
         return view('help.trackstudent');
     }
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+     public function store(Request $request): RedirectResponse
+     {}
 }
