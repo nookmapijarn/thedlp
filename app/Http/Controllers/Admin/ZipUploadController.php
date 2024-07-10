e9ab8e029b7aacdc3ab7fa0931d4ac44f6efb73d
nookmapijarn <nookmapijarn@gmail.com>
2024-07-11 03:44:01 +0700

    fix public path 3

--- app/Http/Controllers/Admin/ZipUploadController.php
+++ app/Http/Controllers/Admin/ZipUploadController.php
@@ -138,7 +138,7 @@ class ZipUploadController extends Controller
             }
 
             // Process the GROUP.dbf file
-            $groupDbfPath = public_path('storage/uploads/unzipped/GROUP.dbf');//"{$extractPath}/{$this->sc_code}/GROUP.dbf";
+            $groupDbfPath = public_path("storage\uploads\unzipped/{$this->sc_code}/GROUP.dbf");//"{$extractPath}/{$this->sc_code}/GROUP.dbf";
 
             // ตรวจสอบว่าไฟล์มีอยู่หรือไม่ก่อนทำการเปลี่ยนแปลงสิทธิ์
             if (File::exists($groupDbfPath)) {
@@ -187,7 +187,7 @@ class ZipUploadController extends Controller
 
         foreach ($files as $file) {
 
-            $dbfPath = public_path("storage/uploads/unzipped/{$extractPath}/{$sc_code}/$level/{$file}.dbf");//"{$extractPath}/{$sc_code}/$level/{$file}.dbf";
+            $dbfPath = public_path("storage\uploads\unzipped/{$sc_code}/{$level}/{$file}.dbf");//"{$extractPath}/{$sc_code}/$level/{$file}.dbf";
 
             // ตรวจสอบว่าไฟล์มีอยู่หรือไม่ก่อนทำการเปลี่ยนแปลงสิทธิ์
             if (File::exists($dbfPath)) {
